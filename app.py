f53ceb4609cd65197baf0c9803d23d32b438c02c
Irfa Ahmad <irfa.ahmed.31@gmail.com>
2021-06-20 23:36:30 -0400

    organized in one file

--- app.py
+++ app.py
@@ -1,57 +1,85 @@
 from tkinter import *
-import database
+from database import *
+import sqlite3 as sq
 
+# database commands
+CREATE_HW_TABLE = """
+CREATE TABLE IF NOT EXISTS homework
+(id INTEGER PRIMARY KEY, course TEXT, type TEXT, time INTEGER)"""
 
-def addHw(course, type, time):
-    connection = database.connect()
-    database.create_table(connection)
-    database.add_hw(connection, course, type, time)
+INSERT_HW = """
+INSERT INTO homework (course, type, time)
+ VALUES (?, ?, ?)"""
 
-def whatToWorkOn():
-    output = Text(window)
-    output.grid(row=9)
-    output.delete(0.0, END)
-    connection = database.connect()
-    try:
-        hw = database.get_hw(connection)
-        answer = "you should work on your " + hw[2] +" for "+ hw[1] +"!"
-    except:
-        answer = "you don't have any homework!"
-    output.insert(END, answer)
+SHORTEST_HW = """
+SELECT * FROM homework
+ORDER BY time DESC 
+LIMIT 1"""
 
-# main:
+# database connection
+con = sq.connect("data.db", timeout=10)
+c = con.cursor()
+
+# set GUI
 window = Tk()
-window.title("Homework Tracker")
-window.configure(background="pink")
-window.geometry('600x600')
-window.resizable(0,0)
+window.title("Homework Suggester")
+window.geometry("800x600+0+0")
+header = Label(window, text="Your Homework Suggester", font=("arial", 30, "bold"),
+               fg="goldenrod").pack()
 
-# title
-appTitle = Label(window, text="Here's your own Homework Tracker!",
-                 bg="pink", fg="white", font="arial 20 bold")
-appTitle.grid(row=0, column=1, columnspan=1, pady=5)
+# variable
+course = StringVar(window)
+hwType = StringVar(window)
+time = IntVar(window)
+Answer = StringVar(window)
 
-# client options
-options = Label(window, text="add homework if you'd like, or find the next piece of homework you should complete!",
-                fg="white", bg="pink", font="arial 13 bold")
-options.grid(row=1, column=1, columnspan=2, pady=10)
+# clear variables
+def clear():
+    course.set("")
+    hwType.set("")
+    time.set(0)
+    Answer.set("")
 
-# text entries
-course_box = Entry(window, width=30, bg="white", fg="pink")
-course_box.grid(row=4)
-course = course_box.get()
-type_box = Entry(window, width=30, bg="white", fg="pink")
-type_box.grid(row=5)
-hw_type = type_box.get()
-time_box = Entry(window, width=30, bg="white", fg="pink")
-time_box.grid(row=6)
-time = time_box.get()
+# add homework into database
+def addHw(course, type, time):
+    c.execute(CREATE_HW_TABLE)
+    c.execute(INSERT_HW, (course, type, time))
+    con.commit()
+    clear()
+
+# insert homework into
+def insertHw():
+    hwAnswer = Text(window)
+    hwAnswer.delete(0.0, END)
+    try:
+        hw = c.execute(SHORTEST_HW).fetchall()
+        answer = "You should work on your " + hw[2] + " for "+ hw[1] +"!"
+    except:
+        answer = "Add homework to find out what to work on next!"
+    Answer.set(answer)
+    hwAnswer.insert(INSERT, Answer)
+    clear()
+
+# labels
+courseL = Label(window, text="Course", font=("arial", 20), fg="goldenrod").place(x=10, y=100)
+hwTypeL = Label(window, text="Homework Type", font=("arial", 20), fg="goldenrod").place(x=10, y=150)
+timeL = Label(window, text="Time required (hours)", font=("arial", 20), fg="goldenrod").place(x=10, y=200)
+
+# entry widgets
+courseT = Entry(window, textvariable=course)
+courseT.place(x=220, y=105)
+hwTypeT = Entry(window, textvariable=hwType)
+hwTypeT.place(x=220, y=155)
+timeT = Entry(window, textvariable=time)
+timeT.place(x=220, y=205)
 
 # buttons
-submit = Button(window, text="Add Homework!", command=addHw(course, hw_type, time), fg="white")
-submit.grid(row=7, column=1)
-getHW = Button(window, text="What to Work on Next", command=whatToWorkOn())
-getHW.grid(row=8, column=1, padx=50, pady=5)
-
+addButton = Button(window, text="Add homework",
+                   command=addHw(course.get(), hwType.get(), time.get()))
+addButton.place(x=10, y=300)
+getButton = Button(window, text="Get homework", command=insertHw())
+getButton.place(x=10, y=350)
+clearButton = Button(window, text="Clear", command=clear())
+clearButton.place(x=10, y=400)
 
 window.mainloop()
